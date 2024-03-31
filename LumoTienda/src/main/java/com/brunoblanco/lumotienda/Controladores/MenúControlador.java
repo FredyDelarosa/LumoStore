1025206e1a8d454dfa25210e0c782968554c170f
BrunoBlanco011 <brunorockwhite@gmail.com>
2024-03-30 19:53:47 -0600

    tercer commit

---
+++ LumoTienda/src/main/java/com/brunoblanco/lumotienda/Controladores/MenúControlador.java
@@ -0,0 +1,41 @@
+package com.brunoblanco.lumotienda.Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class MenúControlador {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void CompraButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ProductoButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ReporteButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void VentaButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
