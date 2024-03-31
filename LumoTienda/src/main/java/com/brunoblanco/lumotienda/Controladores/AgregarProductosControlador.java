90194d4d3ffdfa850f6d5df54e28d753c60ea034
BrunoBlanco011 <brunorockwhite@gmail.com>
2024-03-30 20:15:49 -0600

    quinto commit

---
+++ LumoTienda/src/main/java/com/brunoblanco/lumotienda/Controladores/AgregarProductosControlador.java
@@ -0,0 +1,46 @@
+package com.brunoblanco.lumotienda.Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class AgregarProductosControlador {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void CantidadText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void CategoriaText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void MenuButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void NombreText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void PrecioText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
