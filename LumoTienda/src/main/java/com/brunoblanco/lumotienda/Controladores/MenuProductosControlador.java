edc0e22b295835a314c0efde71aec904fe07fda2
BrunoBlanco011 <brunorockwhite@gmail.com>
2024-03-30 20:07:00 -0600

    cuarto commit

---
+++ LumoTienda/src/main/java/com/brunoblanco/lumotienda/Controladores/MenuProductosControlador.java
@@ -0,0 +1,51 @@
+package com.brunoblanco.lumotienda.Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class MenuProductosControlador {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    void ActualizarProductoButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void AgregarProductoButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void BuscarProductoButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EliminarProductoButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void MenuButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert pane != null : "fx:id=\"pane\" was not injected: check your FXML file 'MenúProductos.fxml'.";
+
+    }
+
+}
---
+++ ss
@@ -0,0 +1,23 @@
+[33mcommit 1025206e1a8d454dfa25210e0c782968554c170f[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: BrunoBlanco011 <brunorockwhite@gmail.com>
+Date:   Sat Mar 30 19:53:47 2024 -0600
+
+    tercer commit
+
+[33mcommit 5b8a392cc745c018d2f8cf3e951765aa8cc9fc68[m
+Author: BrunoBlanco011 <brunorockwhite@gmail.com>
+Date:   Sat Mar 30 19:21:56 2024 -0600
+
+    segundo commit
+
+[33mcommit 7996e6129c7d25dfcbf2cdf60361a76224f8cba5[m
+Author: BrunoBlanco011 <brunorockwhite@gmail.com>
+Date:   Sat Mar 30 19:12:54 2024 -0600
+
+    primer commit
+
+[33mcommit 2cfc9cbb2931b3fa9bb35683c100ba5571786efa[m
+Author: FredyDelarosa <107330052+FredyDelarosa@users.noreply.github.com>
+Date:   Wed Mar 27 11:51:02 2024 -0600
+
+    Initial commit
