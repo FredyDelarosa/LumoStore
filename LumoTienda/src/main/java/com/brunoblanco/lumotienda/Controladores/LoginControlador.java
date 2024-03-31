5b8a392cc745c018d2f8cf3e951765aa8cc9fc68
BrunoBlanco011 <brunorockwhite@gmail.com>
2024-03-30 19:21:56 -0600

    segundo commit

---
+++ LumoTienda/src/main/java/com/brunoblanco/lumotienda/Controladores/LoginControlador.java
@@ -0,0 +1,36 @@
+package com.brunoblanco.lumotienda.Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class LoginControlador {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    void contraseñaText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void usuarioText(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert img != null : "fx:id=\"img\" was not injected: check your FXML file 'Login.fxml'.";
+
+    }
+
+}
