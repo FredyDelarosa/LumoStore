86e8a02998eade39c975a56097b7aa36177954c7
FredyDelarosa <fredydelarosarojascetis138@gmail.com>
2024-04-05 11:00:06 -0600

    commit 25

--- LumoTienda/src/main/java/com/brunoblanco/lumotienda/Controladores/MenuProveedorControlador.java
+++ LumoTienda/src/main/java/com/brunoblanco/lumotienda/Controladores/MenuProveedorControlador.java
@@ -1,12 +1,19 @@
 package com.brunoblanco.lumotienda.Controladores;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.brunoblanco.lumotienda.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class MenuProveedorControlador {
 
@@ -27,17 +34,53 @@ public class MenuProveedorControlador {
 
     @FXML
     void apartarProductoButton(MouseEvent event) {
-
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ApartarProductos.fxml"));
+        try {
+            Pane root = fxmlLoader.load();
+            Scene scene= new Scene(root);
+            stage.setTitle("Apartar Productos");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Node source = (Node) event.getSource();
+        stage = (Stage) source.getScene().getWindow();stage.close();
     }
 
     @FXML
     void comprarProductoButton(MouseEvent event) {
-
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ComprarApartados.fxml"));
+        try {
+            Pane root = fxmlLoader.load();
+            Scene scene= new Scene(root);
+            stage.setTitle("Comprar Apartados");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Node source = (Node) event.getSource();
+        stage = (Stage) source.getScene().getWindow();stage.close();
     }
 
     @FXML
     void menubutton(MouseEvent event) {
-
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Menú.fxml"));
+        try {
+            Pane root = fxmlLoader.load();
+            Scene scene= new Scene(root);
+            stage.setTitle("Menú");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Node source = (Node) event.getSource();
+        stage = (Stage) source.getScene().getWindow();stage.close();
     }
 
     @FXML
